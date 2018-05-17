c0d930d46529c010cc5d3072b879a58a692c9c7f
gyygyy32 <Administrator@DESKTOP-7KNBVD8>
2018-05-17 23:22:41 +0800

    1

---
+++ JHReport/Controllers/Measurement/MeasurementController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JHReport.Controllers.Measurement
+{
+    [RoutePrefix("Measurement")]
+    public class MeasurementController : Controller
+    {
+        [Route("{action=Measure}")]
+        // GET: Measure
+        public ActionResult Measure()
+        {
+            return View();
+        }
+    }
+}
