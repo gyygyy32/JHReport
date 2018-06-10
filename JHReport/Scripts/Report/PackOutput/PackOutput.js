10eec35247c09678174e5fc7e37b24bc5701e94b
gyygyy32 <Administrator@DESKTOP-7KNBVD8>
2018-06-10 22:51:14 +0800

    1
    1

---
+++ JHReport/Scripts/Report/PackOutput/PackOutput.js
@@ -0,0 +1,362 @@
+﻿
+$().ready(function () {
+    $('.input-daterange').datepicker({
+        language: 'zh-CN',//显示中文
+        format: 'yyyy-mm-dd',
+    });
+    $('#txtEndtime').val(moment().format("YYYY-MM-DD"));
+    $('#txtBegintime').val(moment().subtract(1, 'days').format("YYYY-MM-DD"));
+    $('#ddlWorkshop').val('');
+    $('#ddlWorkshop').select2({
+        placeholder: "车间",
+        allowClear: true
+    });
+    
+    //1.初始化Table
+    var oTable = new TableInit();
+    oTable.Init();
+
+})
+function QueryddlStatus() {
+    var promiseweld = $.ajax({
+        url: '../api/QC/QueryStatus',
+        type: 'post',
+        cache: true,
+        async: true,
+    });
+    promiseweld.done(function (r) {
+        $.each(r, function (index, value) {
+            $("#ddlStatus").append("<option value='" + value.visit_type + "'>" + value.visit_type_desc + "</option>");
+        })
+        $('#ddlStatus').val('');
+        $('#ddlStatus').select2({
+            placeholder: "状态",
+            allowClear: true
+        });
+    });
+    promiseweld.fail(function (error) {
+        console.log(error)
+        alert(error);
+    });
+}
+
+
+$('#btnQuery').click(function () {
+
+    $("#tbqc").bootstrapTable('refresh');
+
+})
+var TableInit = function () {
+    var oTableInit = new Object();
+    //初始化Table
+    oTableInit.Init = function () {
+        $('#tbqc').bootstrapTable({
+            url: '../api/QC/QueryQCInfo',         //请求后台的URL（*）
+            method: 'post',                      //请求方式（*）
+            toolbar: '#toolbar',                //工具按钮用哪个容器
+            striped: true,                      //是否显示行间隔色
+            cache: false,                       //是否使用缓存，默认为true，所以一般情况下需要设置一下这个属性（*）
+            pagination: true,                   //是否显示分页（*）
+            sortable: false,                     //是否启用排序
+            sortOrder: "asc",                   //排序方式
+            queryParams: oTableInit.queryParams,//传递参数（*）
+            sidePagination: "client",           //分页方式：client客户端分页，server服务端分页（*）
+            contentType: 'application/json',
+            pageNumber: 1,                       //初始化加载第一页，默认第一页
+            pageSize: 20,                       //每页的记录行数（*）
+            pageList: [10, 25, 50, 100],        //可供选择的每页的行数（*）
+            search: true,                       //是否显示表格搜索，此搜索是客户端搜索，不会进服务端，所以，个人感觉意义不大
+            strictSearch: true,
+            showColumns: true,                  //是否显示所有的列
+            showRefresh: true,                  //是否显示刷新按钮
+            minimumCountColumns: 2,             //最少允许的列数
+            clickToSelect: true,                //是否启用点击选中行
+            //height: 500,                        //行高，如果没有设置height属性，表格自动根据记录条数觉得表格高度
+            //uniqueId: "aiid",                     //每一行的唯一标识，一般为主键列
+            showToggle: true,                    //是否显示详细视图和列表视图的切换按钮
+            cardView: false,                    //是否显示详细视图
+            detailView: false,                   //是否显示父子表
+            showExport: true,                     //是否显示导出
+            exportDataType: "all",              //basic', 'all', 'selected'.
+            exportTypes: ['json', 'xml', 'csv', 'txt', 'sql', 'excel'],
+
+
+            onLoadSuccess: function (data) {
+                var data = $('#tbqc').bootstrapTable('getData', true);
+                //合并单元格
+                mergeCells(data, "serial_nbr", 1, $('#tbqc'));
+                mergeCells(data, "schedule_nbr", 1, $('#tbqc'));
+
+            },
+            columns: [{
+                checkbox: true
+            }, {
+                    field: 'pallet_nbr',
+                title: '托盘号'
+            }, {
+                    field: 'container_nbr',
+                title: '柜号'
+            }, {
+                    field: 'serial_nbr',
+                    title: '组件序列号'
+            }, {
+                    field: 'workorder',
+                    title: '工单号'
+            },
+            {
+                field: 'power_grade',
+                title: '功率档位'
+            },
+            {
+                field: 'current_grade',
+                title: '电流档位'
+            },
+            {
+                field: 'product_code',
+                title: '装配件号'
+            }, {
+                field: 'el_grade',
+                title: 'EL等级'
+            },
+            {
+                field: "final_grade",
+                title: "外观等级"
+            },
+            {
+                field: "pack_date",
+                title: "封箱时间"
+                },
+                {
+                    field: "descriptions",
+                    title: "功率组"
+                },
+                {
+                    field: "pallet_status_desc",
+                    title: "是否入库"
+                },
+                {
+                    field: "shift_type",
+                    title: "班次"
+                },
+                {
+                    field: "pmax",
+                    title: "Pmax"
+                },
+                {
+                    field: "voc",
+                    title: "VOC"
+                },
+                {
+                    field: "ISC",
+                    title: "ISC"
+                },
+                {
+                    field: "ff",
+                    title: "FF"
+                },
+                {
+                    field: "vpm",
+                    title: "VPM"
+                },
+                {
+                    field: "ipm",
+                    title: "IPM"
+                },
+                {
+                    field: "rs",
+                    title: "RS"
+                },
+                {
+                    field: "rsh",
+                    title: "RSH"
+                },
+                {
+                    field: "rsh",
+                    title: "RSH"
+                },
+                {
+                    field: "eff",
+                    title: "EFF"
+                },
+                {
+                    field: "env_temp",
+                    title: "ENV_TEMP"
+                },
+                {
+                    field: "surf_temp",
+                    title: "SURF_TEMP"
+                },
+                {
+                    field: "temp",
+                    title: "TEMP"
+                },
+                {
+                    field: "ivfile_path",
+                    title: "IVFILE_PATH"   
+                },
+                {
+                    field: "pack_seq",
+                    title: "包装顺序" 
+                },
+                {
+                    field: "check_nbr",
+                    title: "报检单号" 
+                },
+                {
+                    field: "cell_supplier",
+                    title: "电池片厂商"  
+                },
+                {
+                    field: "",
+                    title: "电池片档位"  
+                },
+                {
+                    field: "cell_uop",
+                    title: "单片功率"  
+                },
+                {
+                    field: "fff",
+                    title: "转换效率"  
+                },
+                {
+                    field: "eva_supplier_code",
+                    title: "EVA厂商"  
+
+                },
+                {
+                    field: "eva_thickness",
+                    title:"普通EVA规格"
+                },
+                {
+                    field: "eva_lot_nbr",
+                    title: "普通EVA批号"
+                },
+                {
+                    field: "bks_supplier_code",
+                    title: "背板厂商"
+                },
+                {
+                    field: "bks_thickness",
+                    title: "背板规格"
+                },
+                {
+                    field: "bks_lot_nbr",
+                    title: "背板批号"
+                },
+                {
+                    field: "glass_supplier_code",
+                    title: "玻璃供应商"
+                }, {
+                    field: "glass_thickness_desc",
+                    title: "玻璃规格"
+                },
+                {
+                    field: "glass_lot_nbr",
+                    title: "玻璃批号"
+                },
+                {
+                    field: "jbox_supplier_code",
+                    title: "接线盒供应商"
+                },
+                {
+                    field: "jbox_model_desc",
+                    title: "接线盒规格"
+                },
+                {
+                    field: "jbox_lot_nbr",
+                    title: "接线盒批号"
+                },
+                {
+                    field: "frame_supplier_code",
+                    title: "型材供应商"
+                },
+                {
+                    field: "frame_thickness_desc",
+                    title: "型材规格"
+                },
+                {
+                    field: "frame_lot_nbr",
+                    title: "型材批号"
+                },
+                {
+                    field: "customer",
+                    title: "客户"
+                },
+                {
+                    field: "product_code_original",
+                    title: "产品组"
+                }
+
+
+            ]
+        });
+    };
+
+    //得到查询的参数
+    oTableInit.queryParams = function (params) {
+        var temp = {   //这里的键的名字和控制器的变量名必须一直，这边改动，控制器也需要改成一样的
+            limit: params.limit,   //页面大小
+            offset: params.offset,  //页码
+            //departmentname: $("#txt_search_departmentname").val(),
+            //statu: $("#txt_search_statu").val()
+            begintime: $('#txtBegintime').val(),
+            endtime: $('#txtEndtime').val(),
+            workshop: $('#ddlWorkshop').val(),
+            containerno: $('#ddlStatus').val(),
+            lotno: $('#LotID').val(),
+
+        };
+        return temp;
+    };
+    return oTableInit;
+};
+
+
+var ButtonInit = function () {
+    var oInit = new Object();
+    var postdata = {};
+
+    oInit.Init = function () {
+        //初始化页面上面的按钮事件
+    };
+
+    return oInit;
+};
+
+/**
+ * 合并单元格
+ * @param data  原始数据（在服务端完成排序）
+ * @param fieldName 合并属性名称
+ * @param colspan   合并列
+ * @param target    目标表格对象
+ */
+function mergeCells(data, fieldName, colspan, target) {
+    //声明一个map计算相同属性值在data对象出现的次数和
+    var sortMap = {};
+    for (var i = 0; i < data.length; i++) {
+        for (var prop in data[i]) {
+            if (prop == fieldName) {
+                var key = data[i][prop]
+                if (sortMap.hasOwnProperty(key)) {
+                    sortMap[key] = sortMap[key] * 1 + 1;
+                } else {
+                    sortMap[key] = 1;
+                }
+                break;
+            }
+        }
+    }
+    for (var prop in sortMap) {
+        console.log(prop, sortMap[prop])
+    }
+    var index = 0;
+    for (var prop in sortMap) {
+        var count = sortMap[prop] * 1;
+        $(target).bootstrapTable('mergeCells', { index: index, field: fieldName, colspan: colspan, rowspan: count });
+        index += count;
+    }
+}
+
+
+
+
---
+++ JHReport/WebApi/Report/PackOutputController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+//using System.Web.Mvc;
+using Dapper;
+namespace JHReport.WebApi.Report
+{
+    [RoutePrefix("api/PackOutput")]
+    public class PackOutputController : ApiController
+    {
+        [Route("QueryInfo")]
+        [HttpPost]
+        public IHttpActionResult QueryQCInfo(dynamic para)
+        {
+            string sql = @"SELECT ast.[pallet_nbr]/*托盘编码*/
+,ppk.container_nbr/*柜号*/
+      ,ast.[serial_nbr] /*组件序列号*/
+      ,ast.[workorder]/*工单号*/
+      ,ast.[power_grade]/*功率档位*/
+      ,ast.current_grade/*电流档位*/
+       ,ast.[product_code]/*装配件号*/
+       ,ast.[el_grade]/*EL等级*/
+      ,ast.[final_grade]/*外观等级*/
+     -- ,ast.[packing_date]/*封箱时间*/
+      ,ppk.[pack_date]/*封箱时间*/
+       ,cpp.[descriptions]/*功率组*/
+       ,dps.[pallet_status_desc]/*是否入库*/
+        ,dst.[descriptions] shift_type/*班次*/
+         ,iv.[pmax]
+      ,iv.[voc]
+      ,iv.[isc]
+      ,iv.[ff]
+      ,iv.[vpm]
+      ,iv.[ipm]
+      ,iv.[rs]
+      ,iv.[rsh]
+      ,iv.[eff]
+      ,iv.[env_temp]
+      ,iv.[surf_temp]
+      ,iv.[temp]
+      ,iv.[ivfile_path] 
+     ,ast.[pack_seq]
+     ,ppk.[check_nbr]
+     ,ms.[descriptions] as cell_supplier/*电池片供应商*/
+     ,'' /*电池片档位*/
+      ,[cell_uop] /*电池片效率*/
+      ,iv.[pmax]/(cast(REPLACE(cp.[cell_qty],'W','') as  float)*cast(REPLACE([cell_uop],'W','') as  float)) fff /*转换效率*/
+      --,cp.[cell_qty]
+      ,ab.[eva_supplier_code]/*EVA供应商*/
+      ,cmeva.[eva_thickness] /*EVA规格*/
+      ,ab.[eva_lot_nbr]/*EVA批号*/
+       ,ab.[bks_supplier_code]/*背板供应商*/
+       ,cmbks.[bks_thickness] /*bks规格*/
+      ,ab.[bks_lot_nbr]/*背板批号*/
+      ,ab.[glass_supplier_code]/*玻璃供应商*/
+      ,cmglass.glass_thickness_desc /*玻璃规格*/
+      ,ab.[glass_lot_nbr]/*电池批号*/
+      ,ab.[jbox_supplier_code]/*接线盒供应商*/
+      ,cmjbox.jbox_model_desc/*型材规格*/
+      ,ab.[jbox_lot_nbr]/*接线盒批号*/
+      ,ab.[frame_supplier_code]/*型材供应商*/
+      ,cmframe.frame_thickness_desc/*型材规格*/
+      ,ab.[frame_lot_nbr]/*型材批号*/
+      ,wm.[customer]
+      ,ppk.[product_code_original]
+  FROM [mes_main].[dbo].[assembly_status] ast
+  left join [mes_level2_iface].[dbo].[iv] iv
+  on ast.[serial_nbr]=iv.[serial_nbr]
+  left join [mes_main].[dbo].[pack_pallets] ppk
+  on ast.[pallet_nbr]=ppk.[pallet_nbr]
+  left join [mes_main].[dbo].[df_pallet_status] dps
+  on ppk.[pallet_status]=dps.[pallet_status]
+  left join [mes_main].[dbo].[df_shift_type] dst
+  on ppk.[shift_type]=dst.[shift_type]
+  left join [mes_main].[dbo].[config_power_group] cpp
+  on ppk.[power_grade_group_id]=cpp.[power_grade_group_id]
+  left join [mes_main].[dbo].[assembly_basis] ab
+  on ast.[serial_nbr]=ab.[serial_nbr]
+  left join [mes_main].[dbo].[config_mat_cell] cmc
+  on  ab.[cell_part_nbr]=cmc.[part_nbr]
+  left join [mes_level4_iface].[dbo].[master_supplier] ms
+  on ab.[cell_supplier_code]=ms.[supplier_code]
+  left join [mes_main].[dbo].[config_products] cp
+  on ab.[product_code]=cp.[product_code]
+  left join  [mes_main].[dbo].[wo_mfg] wm
+  on ast.[workorder]=wm.[workorder]
+  left join [mes_main].[dbo].[config_mat_eva] cmeva
+  on ab.[eva_part_nbr]=cmeva.[part_nbr]
+  left join [mes_main].[dbo].[config_mat_bks] cmbks
+  on ab.[cell_part_nbr]=cmbks.[part_nbr]
+  left join [mes_main].[dbo].[config_mat_glass] cmglass
+  on ab.glass_part_nbr=cmglass.part_nbr
+  left join [mes_main].[dbo].[config_mat_frame] cmframe
+  on ab.frame_part_nbr=cmframe.part_nbr
+  left join [mes_main].[dbo].[config_mat_jbox] cmjbox
+  on ab.jbox_part_nbr=cmjbox.part_nbr
+where ppk.[pack_date]>='" + Convert.ToString(para.begintime) + "' and ppk.[pack_date]<='" + Convert.ToString(para.endtime) + "' ";
+            //柜号
+            sql += String.IsNullOrEmpty(para.containerno.ToString()) ? "" : " and ppk.[container_nbr] LIKE '" + Convert.ToString(para.containerno) + "%'";
+            //托盘号
+            sql += String.IsNullOrEmpty(para.palletno.ToString()) ? "" : " and ast.[pallet_nbr] LIKE'" + Convert.ToString(para.palletno) + "%' ";
+            //组件序列号
+            sql += String.IsNullOrEmpty(para.lotno.ToString()) ? "" : " AND ast.[serial_nbr] = '" + Convert.ToString(para.lotno) + "'";
+            //报检单号
+            sql += String.IsNullOrEmpty(para.checkno.ToString()) ? "" : " AND ast.[serial_nbr] = '" + Convert.ToString(para.lotno) + "'";
+
+            IEnumerable<dynamic> res = null;
+            using (var conn = Dpperhelper.OpenSqlConnection())
+            {
+                res = conn.Query(sql, new { lotid = Convert.ToString(para.lotid) });
+            }
+            return Json<dynamic>(res);
+            //return Json<dynamic>(new { AA = "aa", BB = "cc" });
+        }
+
+        [Route("QueryStatus")]
+        [HttpPost]
+        public IHttpActionResult QueryStatus()
+        {
+            string sql = @"select * from [mes_main].[dbo].[df_wks_visit_type] vt
+where vt.visit_type in ('M','H','RL','S')";
+            IEnumerable<dynamic> res = null;
+            using (var conn = Dpperhelper.OpenSqlConnection())
+            {
+                res = conn.Query(sql);
+            }
+            return Json<dynamic>(res);
+
+
+        }
+        //[Route("QueryStatus")]
+        //[HttpPost]
+        //public FileResult tt()
+        //{
+        //    return FileResult();
+        //}
+    }
+}
